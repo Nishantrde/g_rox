3b16d3d1dd194d23d4f31c3d4d48cb869dcf6c99
Nishant Garg <nishant2024rde@gmail.com>
2026-05-31 16:22:08 +0530

    refactor: reorganize middleware for logging requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,9 +7,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// Add this to see incoming requests in 'pm2 logs'
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+// Match both with and without trailing slash
 app.use("/api/users", userRoutes)
 
 export default app
-
-
-
